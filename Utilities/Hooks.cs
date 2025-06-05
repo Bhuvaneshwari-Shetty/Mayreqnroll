026e8779487df1f263bc60f1d6e19b85dee3b800
Bhuvaneshwari Shetty <bhush@eg.dk>
2025-06-05 14:46:58 +0530

    Committed

---
+++ Utilities/Hooks.cs
@@ -0,0 +1,104 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using Reqnroll.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace ReqnrollProject.Utilities
+{
+    [Binding]
+    internal class Hooks
+    {
+
+        // private readonly ScenarioContext _scenarioContext;
+
+        private readonly DriverContext _context;
+
+        public Hooks(DriverContext context)
+        {
+            _context = context;
+        }
+
+        [BeforeScenario]
+        public void Setup()
+        {
+
+            Console.WriteLine("Running before every scenario");
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            // IWebDriver driver = new FirefoxDriver();
+            // _context["WebDriver"] = driver;
+
+            _context.Driver = new ChromeDriver();
+            _context.Driver.Manage().Window.Maximize();
+
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+
+            _context.Driver.Quit();
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+
+            Console.WriteLine("Running before TestRun for login functionality");
+
+        }
+
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+
+
+            Console.WriteLine("Running after TestRun for login functionality");
+
+
+        }
+
+        [BeforeStep]
+        public static void BeforeStep()
+        {
+
+            Console.WriteLine("Running before step");
+
+        }
+
+
+        [AfterStep]
+        public static void AfterStep()
+        {
+
+            Console.WriteLine("Running after step");
+
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+
+            Console.WriteLine("Running before every scenario ");
+
+        }
+
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+            Console.WriteLine("Running after every scenario");
+
+        }
+
+
+    }
+}
